7d18884d16bff96a5e6be9962814cd19bed94e1f
kmj525 <kmj525@lehigh.edu>
2024-05-10 17:33:21 -0400

    fixing

--- Bicopter4Full.ino
+++ Bicopter4Full.ino
@@ -38,9 +38,6 @@ const int TIME_STEP_MICRO = 4000;
 int dt = 1000;
 unsigned long clockTime;
 unsigned long printTime;
-unsigned long lastBalloonDetectionTime = 0;
-const unsigned long TURNING_DELAY = 3000; // 3000 milliseconds or 3 seconds
-
 
 int niclaOffset = 11;
 
@@ -63,12 +60,16 @@ void setup() {
     // TODO: make some way to access the actual ground height from robot
     int numSenses = myRobot->sense(senses);
 
+
+
 }
 
 float nicla_yaw = 0;
 float z_estimator = 0;
-bool move = false;
-int forward_force = 0;
+float forward_force = 0;
+unsigned long SpinTimer = millis();
+float SpinYaw = 0.0;
+const float SPIRAL_INCREMENT = 0.08; 
 
 void loop() {
   // Retrieves cmd.params from ground station and checks flags
@@ -99,12 +100,17 @@ void loop() {
     printTime = micros();
   }
 
-
+  const float YAW_INCREMENT = 0.2; // Adjust this value based on desired turning speed
+  unsigned long lastBalloonDetectionTime = 0;
+  const unsigned long TURNING_DELAY = 3000; // 3000 milliseconds or 3 seconds
+  bool shouldStartTurning = false;
 
   // Nicla controller (when the incomming flag = 2)
   if (cmd.params[0] == 2) {
     int nicla_flag = (int)senses[niclaOffset + 0];
     if (nicla_flag != 0) {// checks if new yaw occurs
+        // lastBalloonDetectionTime = millis(); // Update the last detection time
+        // shouldStartTurning = false; // Reset turning flag
         float _yaw = senses[5];  
         float _height = senses[1];  
         float tracking_x = (float)senses[niclaOffset + 1];
@@ -114,78 +120,50 @@ void loop() {
         float des_yaw = ((x_cal - 0.5)) * terms.x_strength; // normalizes the normal to between [-.5, .5] to act as an offset for yaw
         nicla_yaw = _yaw + des_yaw; // add the offset in yaw to the current yaw for movement.
         float y_cal = tracking_y / terms.n_max_y;
-        if (move == false) { // Check if not in moving mode
-         if (abs(x_cal - 0.5) < 0.16 && terms.y_strength != 0) { // makes sure yaw is in center before making height adjustments
-          if (abs(y_cal - terms.y_thresh) > terms.h_ratio) {
-            z_estimator = _height + terms.y_strength * (y_cal - terms.y_thresh); // height control doenst work well when not 0 bouyant
-            // forward_force = 0.16;
-          } 
-          else { // MOVE FORWARD
-            // forward_force = 0;
-            move = true;
-            z_estimator = _height + terms.y_strength * (y_cal - terms.y_thresh);
-          }
+        if ( abs(x_cal - 0.5) < .16 && terms.y_strength != 0) { // makes sure yaw is in center before making height adjustments
+            z_estimator =  ( _height + terms.y_strength * (y_cal - terms.y_thresh)) ; // height control doenst work well when not 0 bouyant
+            forward_force = 0.16;
+        } else {
+          forward_force = 0;
         }
-            lastBalloonDetectionTime = micros(); // last time balloon detected
-            behave.params[1] = terms.fx_togoal; // forward_force
+    } else{ // nical flag does not see red
+      // forward_force = 0.13;
+      // nicla_yaw += YAW_INCREMENT; // Incrementally turn the bicopter
+      // if (nicla_yaw > 360) nicla_yaw -= 360; // Keep the yaw value within 0-360 degrees
+      // if (millis() - lastBalloonDetectionTime > TURNING_DELAY) {
+      //       shouldStartTurning = true;
+      // }
 
-        }
-        else { 
-        // forward_force = 0.13;
-        // nicla_yaw += YAW_INCREMENT; // Incrementally turn the bicopter
-        // if (nicla_yaw > 360) nicla_yaw -= 360; // Keep the yaw value within 0-360 degrees
-        // if (millis() - lastBalloonDetectionTime > TURNING_DELAY) {
-        //       shouldStartTurning = true;
-        // }
-
-        //   if (shouldStartTurning) {
-        //       forward_force = 0.13;
-        //       nicla_yaw += YAW_INCREMENT; // Incrementally turn the bicopter
-        //       if (nicla_yaw > 360) nicla_yaw -= 360; // Normalize yaw
-        //   } else {
-        //       forward_force = 0.1; // Possibly halt movement until turning starts
-        //   }
-        // behave.params[2] = cmd.params[1]; // fz (meters)
-
-        
-        // int dt = SpinTimer - millis();
-        // SpinTimer += dt;
-        // SpinYaw += (float)(1000 / dt) * SPIRAL_INCREMENT;
-        // if (SpinYaw > 1) SpinYaw -= 1;
-        // forward_force = 0.13;
-
-            // moving mode
-            if (micros() - lastBalloonDetectionTime > TURNING_DELAY) {
-                move = false;
-                behave.params[1] = terms.fx_togoal; // forward force
-            } 
-            else { // move but fix height
-                z_estimator = _height + terms.y_strength * (y_cal - terms.y_thresh);
-                behave.params[1] = terms.fx_charge; // forward_force
-            }
-        }
-        behave.params[0] = cmd.params[0]; // flag
-        behave.params[2] = z_estimator; // fz (meters)
-        behave.params[3] = 0; // tx (radians/second)
-        behave.params[4] = nicla_yaw; // tz (radians)
-        }
-      else { // SPIRAL SEARCH
-        float yaw = senses[5] - 0.2; // fix angle of yaw
-        if (yaw < -PI) {
-            yaw = PI;
-        }
-        behave.params[1] = 0.12; // forward_force
-        behave.params[4] = yaw; // angle of yaw
-      } 
-    } else { // direct control with joystick if 'flag' is not 2
-      z_estimator = cmd.params[2];
-      nicla_yaw = cmd.params[4]; // autoset for when switch occurs
-      behave.params[0] = cmd.params[0]; //flag
-      behave.params[1] = cmd.params[1]; //fx
-      behave.params[2] = cmd.params[2]; //fz
-      behave.params[3] = cmd.params[3]; //tx
-      behave.params[4] = cmd.params[4]; //tz
+      //   if (shouldStartTurning) {
+      //       forward_force = 0.13;
+      //       nicla_yaw += YAW_INCREMENT; // Incrementally turn the bicopter
+      //       if (nicla_yaw > 360) nicla_yaw -= 360; // Normalize yaw
+      //   } else {
+      //       forward_force = 0.1; // Possibly halt movement until turning starts
+      //   }
+      // behave.params[2] = cmd.params[1]; // fz (meters)
+      int dt = SpinTimer - millis();
+      SpinTimer += dt;
+      SpinYaw += (float)(1000 / dt) * SPIRAL_INCREMENT;
+      // if (SpinYaw > 1) SpinYaw -= 1;
+      forward_force = 0.13;
     }
+
+    behave.params[0] = cmd.params[0]; // flag
+    behave.params[1] = forward_force; // fx ('meters'/second)
+    behave.params[2] = z_estimator; // fz (meters)
+    behave.params[3] = 0; // tx (radians/second)
+    behave.params[4] = nicla_yaw + SpinYaw; // tz (radians)
+
+  } else { // direct control with joystick if 'flag' is not 2
+    z_estimator = cmd.params[2];
+    nicla_yaw = cmd.params[4]; // autoset for when switch occurs
+    behave.params[0] = cmd.params[0]; //flag
+    behave.params[1] = cmd.params[1]; //fx
+    behave.params[2] = cmd.params[2]; //fz
+    behave.params[3] = cmd.params[3]; //tx
+    behave.params[4] = cmd.params[4]; //tz
+  }
 
   // Send command to the actuators
   myRobot->control(senses, behave.params, 5);
